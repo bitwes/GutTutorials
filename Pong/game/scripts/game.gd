7eaf5bdd32745d294997bb6f6a39e7e5db9e1bbb
Butch Wesley <butch.wesley@gmail.com>
2019-02-17 12:45:22 -0500

    scores added, displaying weird score though

--- Pong/game/scripts/game.gd
+++ Pong/game/scripts/game.gd
@@ -1,6 +1,9 @@
 extends Node2D
-var _p1_score = 0
-var _p2_score = 0
+
+var _scores = {
+	p1 = 0,
+	p2 = 0
+}
 
 func _process(delta):
 	if(Input.is_action_pressed("left_down")):
@@ -13,16 +16,22 @@ func _process(delta):
 	if(Input.is_action_pressed("right_up")):
 		$RightPaddle.up(delta)
 
+func _score(which):
+	_scores[which] += 1
+	if(_scores[which] > 5):
+		_scores[which] = 5
+
 func get_p1_score():
-	return _p1_score
+	return _scores.p1
 
 func get_p2_score():
-	return _p2_score
-
+	return _scores.p2
 
 func _on_ScoreBoxLeft_score():
-	_p2_score += 1
-
-
+	_score('p2')
+	$P2Score.set_text(str(_scores.p2))
+	
 func _on_ScoreBoxRight_score():
-	_p1_score += 1
+	_score('p1')
+	$P1Score.set_text(str(_scores.p1))
+	
--- Pong/game/test/unit/test_game.gd
+++ Pong/game/test/unit/test_game.gd
@@ -3,23 +3,57 @@ extends 'res://addons/gut/test.gd'
 var Game = load('res://scenes/Game.tscn')
 var _game = null
 
+func _p1_score():
+	_game.get_node("ScoreBoxRight").emit_signal('score')
+
+func _p2_score():
+	_game.get_node("ScoreBoxLeft").emit_signal('score')
+
 func before_each():
-    _game = Game.instance()
-    add_child(_game)
+	_game = Game.instance()
+	add_child(_game)
 
 func after_each():
-    remove_child(_game)
+	remove_child(_game)
 
 func test_can_get_p1_score():
-    assert_eq(_game.get_p1_score(), 0)
+	assert_eq(_game.get_p1_score(), 0)
 
 func test_can_get_p2_score():
-    assert_eq(_game.get_p2_score(), 0)
+	assert_eq(_game.get_p2_score(), 0)
 
 func test_when_left_side_scores_p1_score_increased():
-    _game.get_node("ScoreBoxRight").emit_signal('score')
-    assert_eq(_game.get_p1_score(), 1)
+	_p1_score()
+	assert_eq(_game.get_p1_score(), 1)
 
 func test_when_right_side_scores_p2_score_increased():
-    _game.get_node("ScoreBoxLeft").emit_signal('score')
-    assert_eq(_game.get_p2_score(), 1)
+	_p2_score()
+	assert_eq(_game.get_p2_score(), 1)
+
+func test_p2_cannot_score_more_than_5():
+	for i in range(6):
+		_p2_score()
+	assert_eq(_game.get_p2_score(), 5)
+
+func test_p1_cannot_score_more_than_5():
+	for i in range(10):
+		_p1_score()
+	assert_eq(_game.get_p1_score(), 5)
+	
+func test_p1_score_displayed():
+	var label = _game.get_node('P1Score')
+	assert_eq(label.get_text(), '0', 'starts at 0')
+	_p1_score()
+	assert_eq(label.get_text(), '1')
+
+func test_p2_score_displayed():
+	var label = _game.get_node('P2Score')
+	assert_eq(label.get_text(), '0', 'starts at 0')
+	_p2_score()
+	assert_eq(label.get_text(), '1')
+	
+	
+	
+	
+	
+	
