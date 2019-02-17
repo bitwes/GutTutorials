88b13de92eeb16104544a7cb4ddadcaedf38edcd
Butch Wesley <butch.wesley@gmail.com>
2019-02-17 14:51:03 -0500

    refactorings, aligning of all controls in game

--- Pong/game/scripts/game.gd
+++ Pong/game/scripts/game.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-
 onready var _orig_position = $Ball.get_position()
 onready var _p1_default_pos = $LeftPaddle.get_position()
 onready var _p2_default_pos = $RightPaddle.get_position()
@@ -28,13 +27,13 @@ func _reset_positions():
 	_players.p1.paddle.set_position(_p1_default_pos)
 	
 
-func _score(which):
-	_players[which].value += 1
-	if(_players[which].value > 5):
-		_players[which].value = 5
-	_players[which].label.set_text(str(_players[which].value))
+func _score(player):
+	player.value += 1
+	if(player.value > 5):
+		player.value = 5
+	player.label.set_text(str(player.value))
 	_reset_positions()
-	if(which == 'p2'):
+	if(player == _players.p2):
 		$Ball.set_direction(Vector2(1, 0))
 
 func get_p1_score():
@@ -44,10 +43,10 @@ func get_p2_score():
 	return _players.p2.value
 
 func _on_ScoreBoxLeft_score():
-	_score('p2')
+	_score(_players.p2)
 	
 func _on_ScoreBoxRight_score():
-	_score('p1')
+	_score(_players.p1)
 	
 func get_ball():
 	return $Ball
