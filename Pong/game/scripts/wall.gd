4305abd4c96cfff92978acd9ae9eb4d1e9d4a055
Butch Wesley <butch.wesley@gmail.com>
2019-02-16 19:41:48 -0500

    wall, ball scenes and integration tests

--- Pong/game/scripts/wall.gd
+++ Pong/game/scripts/wall.gd
@@ -1,4 +1,9 @@
+extends Area2D
+
 func bounce(ball):
 	var new_dir = ball.get_direction()
 	new_dir.y = new_dir.y * -1
 	ball.set_direction(new_dir)
+
+func _on_Wall_area_entered(area):
+	bounce(area)
