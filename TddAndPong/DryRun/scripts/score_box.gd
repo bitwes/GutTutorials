2a5c03ac27ba8fe73995550e14ce936d18d5e797
Butch Wesley <butch.wesley@gmail.com>
2019-05-19 22:20:26 -0400

    remove dry run

--- TddAndPong/DryRun/scripts/score_box.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-var Ball = load('res://scripts/ball.gd')
-
-signal score
-
-func _on_ScoreBox_area_entered(area):
-	if(area is Ball):
-		emit_signal('score')
--- TddAndPong/DryRun/test/unit/test_score_box.gd
+++
@@ -1,7 +0,0 @@
-extends 'res://addons/gut/test.gd'
-
-var ScoreBox = load('res://scenes/ScoreBox.tscn')
-
-func test_can_make_score_box():
-	assert_not_null(ScoreBox.instance())
-	
