56579f17c4fb92fa21e5409b854721248fe70f65
Butch Wesley <butch.wesley@gmail.com>
2019-11-23 13:12:45 -0500

    score display

--- TddAndPong/Episode4/scripts/game.gd
+++ TddAndPong/Episode4/scripts/game.gd
@@ -4,6 +4,10 @@ var _ball_start_pos = null
 var _p1_score = 0
 var _p2_score = 0
 
+func _update_display():
+	$P1Score.set_text(str(_p1_score))
+	$P2Score.set_text(str(_p2_score))
+
 func _ready():
 	_ball_start_pos =  $Ball.get_position()
 	$Ball.set_speed(300)
@@ -11,6 +15,8 @@ func _ready():
 
 	$P1Paddle.set_speed(300)
 	$P2Paddle.set_speed(300)
+
+	_update_display()
 
 func _process(delta):
 	if Input.is_action_pressed("P1_Up"):
@@ -22,14 +28,21 @@ func _process(delta):
 	if Input.is_action_pressed("P2_Down"):
 		$P2Paddle.move_down(delta)
 
-
 func _on_P1KillBox_kill_ball():
-	$Ball.set_position(_ball_start_pos)
-
+	p2_scores()
 
 func _on_P2KillBox_kill_ball():
-	$Ball.set_position(_ball_start_pos)
+	p1_scores()
 
+func p1_scores():
+	$Ball.set_position(_ball_start_pos)
+	_p1_score += 1
+	_update_display()
+
+func p2_scores():
+	$Ball.set_position(_ball_start_pos)
+	_p2_score += 1
+	_update_display()
 
 func get_p1_score():
 	return _p1_score
--- TddAndPong/Episode4/test/unit/test_game.gd
+++ TddAndPong/Episode4/test/unit/test_game.gd
@@ -1,39 +1,85 @@
 extends 'res://addons/gut/test.gd'
 
-var Game = load('res://scenes/Game.tscn')
+class TestTheBasics:
+	extends 'res://addons/gut/test.gd'
+	var Game = load('res://scenes/Game.tscn')
+
+	var _game = null
+
+	func before_each():
+		_game = Game.instance()
+		add_child(_game)
+
+	func after_each():
+		remove_child(_game)
+
+	func test_can_make_game():
+		assert_not_null(Game.instance())
+
+	func test_get_set_p1_score():
+		assert_accessors(Game.instance(), 'p1_score', 0, 10)
+
+	func test_get_set_p2_score():
+		assert_accessors(Game.instance(), 'p2_score', 0, 10)
+
+	# Does not use p2_score b/c it verifies the wiring of signal.  keep until
+	# wiring tests are broken out.
+	func test_when_p1_kill_box_emits_kill_ball_then_ball_is_recentered():
+		var ball =  _game.get_node("Ball")
+		var p1kb = _game.get_node("P1KillBox")
+		var orig_pos = ball.get_position()
+		ball.set_position(orig_pos + Vector2(100, 100))
+		p1kb.emit_signal('kill_ball')
+
+		assert_eq(ball.get_position(), orig_pos)
+
+	# Does not use p2_score b/c it verifies the wiring of signal.  keep until
+	# wiring tests are broken out.
+	func test_when_p2_kill_box_emits_kill_ball_then_ball_is_recentered():
+		var ball =  _game.get_node("Ball")
+		var p2kb = _game.get_node("P2KillBox")
+		var orig_pos = ball.get_position()
+		ball.set_position(orig_pos + Vector2(100, 100))
+		p2kb.emit_signal('kill_ball')
+
+		assert_eq(ball.get_position(), orig_pos)
+
+	func test_whne_p1_kill_box_kill_ball_then_p2_score_incremented():
+		var p1kb = _game.get_node("P1KillBox")
+		p1kb.emit_signal('kill_ball')
+		assert_eq(_game.get_p2_score(), 1)
+
+	func test_when_p2_kill_box_kill_ball_then_p1_score_incremented():
+		var p2kb = _game.get_node("P2KillBox")
+		p2kb.emit_signal('kill_ball')
+		assert_eq(_game.get_p1_score(), 1)
 
 
-func test_can_make_game():
-	assert_not_null(Game.instance())
 
-func test_get_set_p1_score():
-	assert_accessors(Game.instance(), 'p1_score', 0, 10)
+class TestGUI:
+	extends 'res://addons/gut/test.gd'
 
-func test_get_set_p2_score():
-	assert_accessors(Game.instance(), 'p2_score', 0, 10)
+	var Game = load('res://scenes/Game.tscn')
 
+	var _game = null
 
-func test_when_p1_kill_box_emits_kill_ball_then_ball_is_recentered():
-	var game = Game.instance()
-	add_child(game)
-	var ball =  game.get_node("Ball")
-	var p1kb = game.get_node("P1KillBox")
-	var orig_pos = ball.get_position()
-	ball.set_position(orig_pos + Vector2(100, 100))
-	p1kb.emit_signal('kill_ball')
+	func before_each():
+		_game = Game.instance()
+		add_child(_game)
 
-	assert_eq(ball.get_position(), orig_pos)
-	remove_child(game)
+	func after_each():
+		remove_child(_game)
 
+	func test_when_game_starts_score_display_zero():
+		assert_eq(_game.get_node('P1Score').get_text(), '0', 'p1 score')
+		assert_eq(_game.get_node('P2Score').get_text(), '0', 'p2 score')
 
-func test_when_p2_kill_box_emits_kill_ball_then_ball_is_recentered():
-	var game = Game.instance()
-	add_child(game)
-	var ball =  game.get_node("Ball")
-	var p2kb = game.get_node("P2KillBox")
-	var orig_pos = ball.get_position()
-	ball.set_position(orig_pos + Vector2(100, 100))
-	p2kb.emit_signal('kill_ball')
+	func test_when_p1_scores_score_is_updated():
+		_game.p1_scores()
+		assert_eq(_game.get_node('P1Score').get_text(), '1', 'p1 score')
+		assert_eq(_game.get_node('P2Score').get_text(), '0', 'p2 score')
 
-	assert_eq(ball.get_position(), orig_pos)
-	remove_child(game)
+	func test_when_p2_scores_score_is_updated():
+		_game.p2_scores()
+		assert_eq(_game.get_node('P1Score').get_text(), '0', 'p1 score')
+		assert_eq(_game.get_node('P2Score').get_text(), '1', 'p2 score')
