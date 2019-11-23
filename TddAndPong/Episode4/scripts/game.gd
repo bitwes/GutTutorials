b992d07b70f36c1f9423a797dd107f9908b4bd29
Butch Wesley <butch.wesley@gmail.com>
2019-11-23 13:27:43 -0500

    functional game over

--- TddAndPong/Episode4/scripts/game.gd
+++ TddAndPong/Episode4/scripts/game.gd
@@ -3,6 +3,9 @@ extends Node2D
 var _ball_start_pos = null
 var _p1_score = 0
 var _p2_score = 0
+var _max_score = 10
+
+signal game_over
 
 func _update_display():
 	$P1Score.set_text(str(_p1_score))
@@ -34,17 +37,26 @@ func _on_P1KillBox_kill_ball():
 func _on_P2KillBox_kill_ball():
 	p1_scores()
 
+func _game_over():
+	$Ball.set_speed(0)
+	emit_signal('game_over')
+
+
 func p1_scores():
 	$Ball.set_position(_ball_start_pos)
 	_p1_score += 1
 	$Ball.set_direction(Vector2(1, 0))
 	_update_display()
+	if(_p1_score == _max_score):
+		_game_over()
 
 func p2_scores():
 	$Ball.set_position(_ball_start_pos)
 	_p2_score += 1
 	$Ball.set_direction(Vector2(-1, 0))
 	_update_display()
+	if(_p2_score  == _max_score):
+		_game_over()
 
 func get_p1_score():
 	return _p1_score
@@ -60,3 +72,9 @@ func set_p2_score(p2_score):
 
 func get_ball():
 	return $Ball
+
+func get_max_score():
+	return _max_score
+
+func set_max_score(max_score):
+	_max_score = max_score
--- TddAndPong/Episode4/test/unit/test_game.gd
+++ TddAndPong/Episode4/test/unit/test_game.gd
@@ -22,6 +22,9 @@ class TestTheBasics:
 	func test_get_set_p2_score():
 		assert_accessors(Game.instance(), 'p2_score', 0, 10)
 
+	func test_get_set_max_score():
+		assert_accessors(_game, 'max_score', 10, 20)
+
 	# Does not use p2_score b/c it verifies the wiring of signal.  keep until
 	# wiring tests are broken out.
 	func test_when_p1_kill_box_emits_kill_ball_then_ball_is_recentered():
@@ -66,6 +69,30 @@ class TestTheBasics:
 		_game.get_ball().set_direction(Vector2(0, 0))
 		_game.p1_scores()
 		assert_eq(_game.get_ball().get_direction(), Vector2(1, 0))
+
+	func test_when_p1_reaches_max_score_game_over_signal_emitted():
+		watch_signals(_game)
+		_game.set_max_score(2)
+		_game.p1_scores()
+		_game.p1_scores()
+		assert_signal_emitted(_game, 'game_over')
+
+	func test_when_p2_reaches_max_score_game_over_signal_emitted():
+		watch_signals(_game)
+		_game.set_max_score(1)
+		_game.p2_scores()
+		assert_signal_emitted(_game, 'game_over')
+
+	func test_when_p1_wins_ball_does_not_move():
+		_game.set_max_score(1)
+		_game.p1_scores()
+		assert_eq(_game.get_ball().get_speed(), 0)
+
+	func test_when_p2_wins_ball_does_not_move():
+		_game.set_max_score(1)
+		_game.p2_scores()
+		assert_eq(_game.get_ball().get_speed(), 0)
+
 class TestGUI:
 	extends 'res://addons/gut/test.gd'
 
